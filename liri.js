d12f3f15be7ff161de38843916dbae849ea3b920
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-04 18:32:43 -0700

    Spotify Checking

--- liri.js
+++ liri.js
@@ -1,8 +1,20 @@
-
 // Packages 
 require('dotenv').config()
 const keys = require('./keys')
 const Spotify = require('node-spotify-api');
 const Moment = require('moment')
 const request = require('request')
+
+var spotify = new Spotify(keys.spotify);
+      
+spotify.search({type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+});
+
+// Switch Case
+
 
