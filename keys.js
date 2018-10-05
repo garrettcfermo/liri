51b87dc9682168d499ea34b1ab04a28d73220b5e
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-04 18:02:14 -0700

    Setup API Keys

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+
+// Spotify API Keys
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+}
+
+// OMDB API Keys
+exports.omdb = {
+  id: process.env.OMDB_ID
+}
+
+// Bands in Town API Keys
+exports.bit = {
+  id: process.env.BIT_ID
+}
