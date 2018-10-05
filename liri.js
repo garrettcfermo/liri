0e8d96615140eb217f9011c218338ea009e1df02
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-04 18:10:27 -0700

    Required All Packages to Liri

--- liri.js
+++ liri.js
@@ -1,4 +1,8 @@
-require('dotenv').config()
 
+// Packages 
+require('dotenv').config()
 const keys = require('./keys')
+const Spotify = require('node-spotify-api');
+const Moment = require('moment')
+const request = require('request')
 
