41d94a505945eef0535652f22f4674176518f928
Garrett Fermo <garrettcfermo@gmail.com>
2018-10-06 23:02:32 -0700

    Spotify Function Created

--- liri.js
+++ liri.js
@@ -2,19 +2,47 @@
 require('dotenv').config()
 const keys = require('./keys')
 const Spotify = require('node-spotify-api');
-const Moment = require('moment')
+const moment = require('moment')
 const request = require('request')
+const inquirer = require("inquirer")
+const fs = require('fs')
 
-var spotify = new Spotify(keys.spotify);
-      
-spotify.search({type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+// Current Time Stamp
+timeStamp = moment().format('MMMM Do YYYY, h:mm:ss a')
+
+
+// Spotify Function
+const spotify = song => {
+  var spotify = new Spotify(keys.spotify);
+
+  // Pull Songs API Information 
+  spotify.search({ type: 'track', query: song }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
     }
-   
-  console.log(data); 
-});
+  var results = `
+  Your Spotify Results 
+  _______________________
+  Artist Name : ${data.tracks.items[0].artists[0].name}
+  Album Name : ${data.tracks.items[0].album.name}
+  Song Name : ${data.tracks.items[0].name}
+  Preview : ${data.tracks.items[0].preview_url}
+  Request Time : ${timeStamp}
+  _______________________
+  `
+  console.log(results)
+  fs.appendFile('log.txt', results, function (e) { if (err) { console.log(err) } })
+  });
+}
 
-// Switch Case
+// Concert Function
+
+// Movie Function
+
+// User Interface
+
+const askQuestion = () => {
+
+}
 
 
